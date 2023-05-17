7555e60fae5678f66f89cf262a743f966ccfb509
hare011 <133020000+hare011@users.noreply.github.com>
2023-05-17 13:33:55 -0400

    Update server.py

--- ch11/server.py
+++ ch11/server.py
@@ -1,16 +1,21 @@
 class Module():
+    #Creates a startup script that reconstructs the manual installation commands for the Cloud SQL Auth proxy
     def _startup_script(self):
+        #Sets a variable as the proxy download URL
         proxy_download = 'https://dl.google.com/cloudsql/' + \
             'cloud_sql_proxy.linux.amd64'
+        #Sets a variable that runs the Cloud SQL Auth proxy binary on port 3306
         exec_start = '/usr/local/bin/cloud_sql_proxy ' + \
             '-instances=${google_sql_database_instance.' + \
             f'{self._environment}.connection_name}}=tcp:3306'
-
+        
+        #Returns a shell script that installs the proxy and starts it up with the server
         return f"""
         #!/bin/bash
         wget {proxy_download} -O /usr/local/bin/cloud_sql_proxy
         chmod +x /usr/local/bin/cloud_sql_proxy
         
+        #Configures the systemd daemon to start and stop the Cloud SQL Auth proxy
         cat << EOF > /usr/lib/systemd/system/cloudsqlproxy.service
         [Install]
         WantedBy=multi-user.target
@@ -32,7 +37,7 @@ class Module():
         systemctl daemon-reload
         systemctl start cloudsqlproxy
         """
-
+    #Uses the module to create the JSON configuration for the server
     def __init__(self, service, environment,
                  zone, machine_type='e2-micro'):
         self._name = f'{service}-{environment}'
@@ -40,9 +45,11 @@ class Module():
         self._zone = zone
         self._machine_type = machine_type
 
+    #Uses the module to create the JSON configuration for the Google compute instance and includes a startup script to install the proxy
     def build(self):
         return [
             {
+                #Creates the Google compute instance by using a Terraform resource based on the name, address, region, and network
                 'google_compute_instance': {
                     self._environment: {
                         'allow_stopping_for_update': True,
@@ -62,6 +69,7 @@ class Module():
                                 'network_tier': 'STANDARD'
                             }
                         }],
+                        #The factory for the promotions application’s server uses a service account to access GCP services.
                         'service_account': [{
                             'email': '${google_service_account.' +
                             f'{self._environment}' + '.email}',
