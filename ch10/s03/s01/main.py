18f65ec77174edc341e8bde91e39787316a23651
hare011 <133020000+hare011@users.noreply.github.com>
2023-05-17 13:19:42 -0400

    Update main.py

--- ch10/s03/s01/main.py
+++ ch10/s03/s01/main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         'resource': blue.build()
         + iam.build()
     }
-
+    #Writes the Python dictionary out to a JSON file to be executed by Terraform later
     with open('main.tf.json', 'w') as outfile:
         json.dump(resources, outfile,
                   sort_keys=True, indent=4)
