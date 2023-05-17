acaa62f12dd937795bf6534497ece4efd815adf7
hare011 <133020000+hare011@users.noreply.github.com>
2023-05-17 13:38:49 -0400

    Update test_server.py

--- ch12/s01/test_server.py
+++ ch12/s01/test_server.py
@@ -3,6 +3,7 @@ import os
 import compute
 import json
 
+#Parses and extracts any server JSON configurations across testing and production environments
 ENVIRONMENTS = ['testing', 'prod']
 CONFIGURATION_FILE = 'main.tf.json'
 
@@ -38,14 +39,17 @@ def servers(configuration):
 
 
 def test_cpu_size_less_than_or_equal_to_limit(servers):
-    CPU_LIMIT = 32
-    non_compliant_servers = []
+    CPU_LIMIT = 32 #Sets the CPU limit to 32, the maximum required for the application
+    non_compliant_servers = [] #Initializes a list of noncompliant servers that exceed the 32 vCPU limit
     for server in servers:
+        #For each server configuration, retrieves the machine type attribute and calls the GCP API for more information
         type = compute.get_machine_type(
             PROJECT, server['zone'],
             server['machine_type'])
+        #If the server configuration includes a machine type that exceeds 32 vCPUs, add it to a list of noncompliant servers.
         if type.cpus > CPU_LIMIT:
             non_compliant_servers.append(server['name'])
+    #Checks that all servers comply with the CPU limit. If not, fail the test and throw an error for the servers that exceed 32 CPUs.
     assert len(non_compliant_servers) == 0, \
         f'Servers found using over {CPU_LIMIT}' + \
         f' vCPUs: {non_compliant_servers}'
