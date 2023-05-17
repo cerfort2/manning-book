acbcd97b389b361db6b59b73364914d44adb04e1
hare011 <133020000+hare011@users.noreply.github.com>
2023-05-17 13:18:42 -0400

    Update iam.py

--- ch10/s03/s01/iam.py
+++ ch10/s03/s01/iam.py
@@ -1,9 +1,11 @@
 import os
 
 TEAM = 'sundew'
+#Sets the resource types that Terraform uses as constants so you can reference them later if needed
 TERRAFORM_GCP_SERVICE_ACCOUNT_TYPE = 'google_service_account'
 TERRAFORM_GCP_ROLE_ASSIGNMENT_TYPE = 'google_project_iam_member'
 
+#Keeps all of the users you added to the project as part of the monolith
 users = {
     'audit-team': 'roles/viewer',
     'automation-watering': 'roles/editor',
@@ -20,12 +22,13 @@ def get_user_id(user):
 def build():
     return iam()
 
-
+#Uses the module to create the JSON configuration for the IAM policies outside the monolith
 def iam(users=users):
     iam_members = []
     for user, role in users.items():
         user_id = get_user_id(user)
         iam_members.append({
+            #Creates a GCP service account for the project for each user in the sundew production project
             TERRAFORM_GCP_SERVICE_ACCOUNT_TYPE: [{
                 user_id: [{
                     'account_id': user,
@@ -34,6 +37,7 @@ def iam(users=users):
             }]
         })
         iam_members.append({
+            #Assigns the specific role defined for each service account, such as viewer, editor, or owner
             TERRAFORM_GCP_ROLE_ASSIGNMENT_TYPE: [{
                 user_id: [{
                     'role': role,
