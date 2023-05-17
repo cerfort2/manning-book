3f78e2e1ecd29ec076865c0c3d6a2e38d12a3989
cerfort2 <122559164+cerfort2@users.noreply.github.com>
2023-05-17 12:47:20 -0400

    Update main.py

--- ch05/s01/s02/environments/development/main.py
+++ ch05/s01/s02/environments/development/main.py
@@ -1,6 +1,6 @@
 from tags import StandardTags
 from server import ServerFactoryModule
-from network import NetworkFactoryModule
+from network import NetworkFactoryModule        #imports the modules downloaded by the package manager
 from database import DatabaseFactoryModule
 
 import json
@@ -15,8 +15,10 @@ if __name__ == "__main__":
     database = DatabaseFactoryModule(
         name, server, network, environment, tags.tags)
     resources = {
+  #uses the modules to create the JSON configuration for the network, server, and database
         'resource': network.build() + server.build() + database.build()
     }
 
     with open('main.tf.json', 'w') as outfile:
+        #writes the python dictionary out to a JSON file to be executed by terraform later
         json.dump(resources, outfile, sort_keys=True, indent=4)
