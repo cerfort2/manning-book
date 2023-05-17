532ad946e588de4522b71389b3b87a7c179528fe
cerfort2 <122559164+cerfort2@users.noreply.github.com>
2023-05-17 12:45:18 -0400

    Update main.py

--- ch05/s01/s01/environments/development/main.py
+++ ch05/s01/s01/environments/development/main.py
@@ -1,7 +1,9 @@
 import sys
-sys.path.insert(1, '../../modules/gcp')
 
-# import server and network modules
+#Imports directory with the modules because it exists in the same repository
+sys.path.insert(1, '../../modules/gcp') 
+
+# imports the server, database, and network factory modules for the production envrionment
 from database import DatabaseFactoryModule
 from server import ServerFactoryModule
 from network import NetworkFactoryModule
@@ -16,8 +18,10 @@ if __name__ == "__main__":
     server = ServerFactoryModule(name, environment, network)
     database = DatabaseFactoryModule(name, server, network, environment)
     resources = {
+  #uses the modules to create the JSON configuration for the network, server, and database
         'resource': network.build() + server.build() + database.build()
     }
 
+    #writes the python dictionary out to a json file to be executed by terraform later
     with open('main.tf.json', 'w') as outfile:
         json.dump(resources, outfile, sort_keys=True, indent=4)
