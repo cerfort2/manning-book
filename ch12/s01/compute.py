74ba2ba3ec1c8ca0c7c07a41e45651ab2512a5e6
hare011 <133020000+hare011@users.noreply.github.com>
2023-05-17 13:36:25 -0400

    Update compute.py

--- ch12/s01/compute.py
+++ ch12/s01/compute.py
@@ -1,11 +1,12 @@
 import googleapiclient.discovery
 
-
+#Defines a machine type object to store any attributes you might need to check, including number of vCPUs
 class MachineType():
     def __init__(self, gcp_json):
         self.name = gcp_json['name']
         self.description = gcp_json['description']
         self.cpus = gcp_json['guestCpus']
+        #Converts megabytes of memory to gigabytes for consistent unit measure
         self.ram = self._convert_mb_to_gb(
             gcp_json['memoryMb'])
         self.maxPersistentDisks = gcp_json[
@@ -22,6 +23,7 @@ class MachineType():
 def get_machine_type(project, zone, type):
     service = googleapiclient.discovery.build(
         'compute', 'v1')
+    #Calls the GCP API to retrieve the number of vCPUs for a given machine type
     result = service.machineTypes().list(
         project=project,
         zone=zone,
@@ -29,4 +31,5 @@ def get_machine_type(project, zone, type):
     types = result['items'] if 'items' in result else None
     if len(types) != 1:
         return None
+    #Returns a machine type object with vCPU and disk attributes
     return MachineType(types[0])
