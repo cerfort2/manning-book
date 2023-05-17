987752ee31a247cbf795d871ffa3f074ef331ee9
cerfort2 <122559164+cerfort2@users.noreply.github.com>
2023-05-17 16:57:44 -0400

    Update test_security.py

--- ch08/s03/test_security.py
+++ ch08/s03/test_security.py
@@ -5,20 +5,24 @@ from main import APP_NAME, CONFIGURATION_FILE
 
 @pytest.fixture(scope="module")
 def resources():
+    #Loads the infrastructure configuration from a JSON file
     with open(CONFIGURATION_FILE, 'r') as f:
         config = json.load(f)
     return config['resource']
-
+    #Parses the resource block out of the JSON configuration file
 
 @pytest.fixture
 def database_firewall_rule(resources):
     return resources[0][
         'google_compute_firewall'][0][APP_NAME][0]
+#Parses the Google compute firewall resource defined by Terraform from the JSON configuration
 
-
+#Uses a descriptive test name explaining the policy for the firewall rule, which should not allow all traffic
 def test_database_firewall_rule_should_not_allow_everything(
         database_firewall_rule):
+    #Checks that 0.0.0.0/0, or allow all, is not defined in the rule’s source ranges
     assert '0.0.0.0/0' not in \
+    #Uses a descriptive error message describing how to correct the firewall rule, such as removing 0.0.0.0/0 from source ranges
         database_firewall_rule['source_ranges'], \
         'database firewall rule must not ' + \
         'allow traffic from 0.0.0.0/0, specify source_ranges ' + \
